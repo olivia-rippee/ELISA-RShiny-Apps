4ea7aca0d07aabfcfa6a25dac96bc95a31f643e2
Olivia Rippee <167914008+olivia-rippee@users.noreply.github.com>
2026-08-03 12:56:02 -0500

    Delete Deploy RShiny directory

--- Deploy RShiny/Deploy RShiny.R
+++
@@ -1,7 +0,0 @@
-
-# Publish/redeploy an R Shiny app
-# --------------------------------
-
-setwd("C:/Users/ORippee/OneDrive - Kemin Industries/R&D - Olivia/R Code/FolderofInterest")
-
-rsconnect::deployApp(appDir = ".", appMode = "shiny",  appPrimaryDoc = "app.R")
